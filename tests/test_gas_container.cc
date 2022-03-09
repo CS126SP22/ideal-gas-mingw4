260ddb18c5e208b6e82ea203fdbe8d107821ce39
mingw4 <mingw4@illinois.edu>
2022-03-09 15:19:09 -0600

    Comments on test_gas_container deleted.

--- tests/test_gas_container.cc
+++ tests/test_gas_container.cc
@@ -21,16 +21,3 @@ TEST_CASE("AdvanceOneFrame Test") {
     container.AdvanceOneFrame();
     REQUIRE(container.getFrame() == 1);
 }
-
-/*
-TODO: Rename this test file. You'll also need to modify CMakeLists.txt.
-
-You can (and should) create more test files; this project is too big
-for all tests to be in the same file. Remember that, for each file (foo.cc)
-containing non-trivial code, you should have a corresponding test file
-(foo_test.cc)
-
-Make sure to add any files that you create to CMakeLists.txt.
-
-TODO Delete this comment and the placeholder test before submitting your code.
-*/
