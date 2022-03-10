8a35266634ed9c37ace2e404ca631bb84c20f811
mingw4 <mingw4@illinois.edu>
2022-03-09 21:31:18 -0600

    Roll back done

--- include/gas_simulation_app.h
+++ include/gas_simulation_app.h
@@ -7,23 +7,22 @@
 
 namespace idealgas {
 
-/**
+    /**
  * An app for visualizing the behavior of an ideal gas.
  */
-class IdealGasApp : public ci::app::App {
- public:
-  IdealGasApp();
+    class IdealGasApp : public ci::app::App {
+    public:
+        IdealGasApp();
 
-  void draw() override;
-  void update() override;
+        void draw() override;
+        void update() override;
 
-  // TODO: Delete this comment. Feel free to play around with these variables
-  // provided that you can see the entire UI on your screen.
-  const int kWindowSize = 875;
-  const int kMargin = 100;
 
- private:
-  GasContainer container_;
-};
+        const int kWindowSize = 875;
+        const int kMargin = 100;
+
+    private:
+        GasContainer container_;
+    };
 
 }  // namespace idealgas
