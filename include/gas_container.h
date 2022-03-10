90d4e18adb5f90651b0d6127bcdf6183059b3571
mingw4 <mingw4@illinois.edu>
2022-03-09 21:21:09 -0600

    Roll back due to compling issue

--- include/gas_container.h
+++ include/gas_container.h
@@ -1,9 +1,6 @@
 #pragma once
 
-#include <vector>
-
 #include "cinder/gl/gl.h"
-#include "particle.h"
 
 using glm::vec2;
 
@@ -14,68 +11,29 @@ namespace idealgas {
  * stores all of the particles and updates them on each frame of the simulation.
  */
 class GasContainer {
-    public:
-        std::vector<Particle> particles_;   /*< Vector of particles */
-        /**
-         * Constructs a defualt new container with width of 200 and height of 100.
-         */
-        GasContainer();
+ public:
+  /**
+   * TODO: Add more parameters to this constructor, and add documentation.
+   */
+  GasContainer();
 
-        /**
-         * Constructs a new container with specified length and height.
-         * The frame of the new container is initialized to 0.
-         * @param length The specified length of the container.
-         * @param height The specified height of the container.
-         */
-         GasContainer(double width, double height);
+  /**
+   * Displays the container walls and the current positions of the particles.
+   */
+  void Display() const;
 
-         /**
-          * Gets the frame of the container.
-          * @return The frame of the container.
-          */
-         size_t getFrame();
+  /**
+   * Updates the positions and velocities of all particles (based on the rules
+   * described in the assignment documentation).
+   */
+  void AdvanceOneFrame();
 
-         /**
-          * Gets the width of the container.
-          * @return The width of the container.
-          */
-         double getWidth();
-
-         /**
-          * Gets the height of the container.
-          * @return The height of the container.
-          */
-         double getHeight();
-
-        /**
-         * Displays the container walls and the current positions of the particles.
-         */
-         void Display() const;
-
-        /**
-         * Updates the positions and velocities of all particles (based on the rules
-         * described in the assignment documentation).
-         */
-         void AdvanceOneFrame();
-
-         /**
-          * Add a particle to the gas container.
-          * @param particle The particle to add to the gas container.
-          */
-         void AddParticle(Particle const &particle);
-
-         /**
-          * Calculates the alternation of velocities and positions of all particles prior to frame advance.
-          */
-         void Calculate();
-
-    private:
-        size_t frame_;                      /*< Frame of the container */
-        double width_;                      /*< Width of the container */
-        double height_;                     /*< Height of the container */
-
+ private:
+  /**
+   * This variable is just for the purposes of demonstrating how to make a shape move
+   * across a screen. Please remove it once you start working on your code.
+   */
+  int dummy_variable_ = 0;
 };
-
-
 
 }  // namespace idealgas
--- src/gas_container.cc
+++ src/gas_container.cc
@@ -1,76 +1,23 @@
 #include "gas_container.h"
-#include "particle.h"
 
 namespace idealgas {
 
 using glm::vec2;
 
 GasContainer::GasContainer() {
-    frame_ = 0;
-    this->width_ = 200;
-    this->height_ = 100;
-    this->particles_ = std::vector<Particle>();
-}
 
-GasContainer::GasContainer(double width, double height) {
-    frame_ = 0;
-    this->width_ = width;
-    this->height_ = height;
 }
 
 void GasContainer::Display() const {
   // This function has a lot of magic numbers; be sure to design your code in a way that avoids this.
   ci::gl::color(ci::Color("orange"));
-  ci::gl::drawSolidCircle(vec2(frame_, 200), 10);
+  ci::gl::drawSolidCircle(vec2(dummy_variable_, 200), 10);
   ci::gl::color(ci::Color("white"));
   ci::gl::drawStrokedRect(ci::Rectf(vec2(100, 100), vec2(600, 400)));
 }
 
-size_t GasContainer::getFrame() {
-    return this->frame_;
-}
-
-double GasContainer::getHeight() {
-    return this->height_;
-}
-
-double GasContainer::getWidth() {
-    return this->width_;
-}
-
 void GasContainer::AdvanceOneFrame() {
-  ++frame_;
-}
-void GasContainer::Calculate() {
-    for (std::vector<Particle>::iterator p1 = particles_.begin(); p1 != particles_.end(); ++p1) {
-        //Movement before collision.
-        p1->position_ += p1->velocity_;
-
-        //Check and handle collision with walls.
-
-        //Check collision with left wall
-        if (p1->position_.x - p1->radius_ <= 0 && p1->velocity_.x <0) {
-            p1->velocity_.x = -p1->velocity_.x;
-        }
-        //Check collision with right wall
-        if (p1->position_.x + p1->radius_ >= this->getWidth() && p1->velocity_.x >0) {
-            p1->velocity_.x = -p1->velocity_.x;
-        }
-        //Check collision with bottom wall
-        if (p1->position_.y - p1->radius_ <= 0 && p1->velocity_.y < 0) {
-            p1->velocity_.y = -p1->velocity_.x;
-        }
-        //Check collision with top wall
-        if (p1->position_.y + p1->radius_ >= this->getHeight() && p1->velocity_.y > 0) {
-            p1->velocity_.y = -p1->velocity_.y;
-        }
-
-        //Check and handle collision with other particles
-        for (std::vector<Particle>::iterator p2 = particles_.begin(); p2 != p1; ++p2) {
-
-        }
-
-    }
+  ++dummy_variable_;
 }
 
 }  // namespace idealgas
