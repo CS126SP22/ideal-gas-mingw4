3f741d3e0870d8741eb5d8d731bdc4ea12267343
mingw4 <mingw4@illinois.edu>
2022-03-23 20:49:18 -0500

    Additional tests for handling collision edge.

--- tests/test_HandlingCollisionEdge.cc
+++ tests/test_HandlingCollisionEdge.cc
@@ -35,3 +35,61 @@ TEST_CASE("HandlingCollisionEdge test") {
   REQUIRE(cmpf(new_v_0.x, -1.0f));
   REQUIRE(cmpf(new_v_0.y, -1.0f));
 }
+
+TEST_CASE("HandlingCollisionEdgeCircle test") {
+    GasContainer container;
+
+    container.SetParticlesNum(1);
+    container.SetBoundaryType(true);
+
+    container.SetParticlesPos(vec2(151, 350), 0);
+    container.SetParticlesVelocity(vec2(-1, 0), 0);
+    container.SetParticlesSize(2.0, 0);
+    container.SetParticlesMass(2.0, 0);
+
+    container.AdvanceOneFrame();
+
+    vec2 new_v_0 = container.GetParticlesVelocity(0);
+
+    REQUIRE(cmpf(new_v_0.x, 1.0f));
+    REQUIRE(cmpf(new_v_0.y, 0.0f));
+
+
+    container.SetParticlesPos(vec2(749, 350), 0);
+    container.SetParticlesVelocity(vec2(1, 0), 0);
+    container.SetParticlesSize(2.0, 0);
+    container.SetParticlesMass(2.0, 0);
+
+    container.AdvanceOneFrame();
+
+    new_v_0 = container.GetParticlesVelocity(0);
+
+    REQUIRE(cmpf(new_v_0.x, -1.0f));
+    REQUIRE(cmpf(new_v_0.y, 0.0f));
+
+
+    container.SetParticlesPos(vec2(450, 151), 0);
+    container.SetParticlesVelocity(vec2(0, -1), 0);
+    container.SetParticlesSize(2.0, 0);
+    container.SetParticlesMass(2.0, 0);
+
+    container.AdvanceOneFrame();
+
+    new_v_0 = container.GetParticlesVelocity(0);
+
+    REQUIRE(cmpf(new_v_0.x, 0.0f));
+    REQUIRE(cmpf(new_v_0.y, -1.0f));
+
+    container.SetParticlesPos(vec2(450, 749), 0);
+    container.SetParticlesVelocity(vec2(0, 1), 0);
+    container.SetParticlesSize(2.0, 0);
+    container.SetParticlesMass(2.0, 0);
+
+    container.AdvanceOneFrame();
+
+    new_v_0 = container.GetParticlesVelocity(0);
+
+    REQUIRE(cmpf(new_v_0.x, 0.0f));
+    REQUIRE(cmpf(new_v_0.y, -1.0f));
+
+}
